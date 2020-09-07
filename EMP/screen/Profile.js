fc1802462421f7b91fed93512f3f143f6e87ac2e
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-09-07 12:40:17 +0530

    .

--- EMP/screen/Profile.js
+++ EMP/screen/Profile.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { LinearGradient} from 'expo-linear-gradient';
+import {Title, Card} from 'react-native-paper';
 
 const Profile = () =>{
     return(
@@ -15,12 +16,25 @@ const Profile = () =>{
                 source={{uri:"https://images.unsplash.com/photo-1528158134711-e89943f34728?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80"}}
                 />
             </View>
+            <View style={{alignItems:"center"}} >
+                <Title>Ansari</Title>
+                <Text>WebDev</Text>
+            </View>
+            <Card style={styles.mycard}>
+
+            </Card>
         </View>
     )
 }
 const styles = StyleSheet.create({
     root:{
         flex:1
+    },
+    mycard:{
+        margin:3
+    },
+    cardcontent:{
+        flexDirection:"row"        
     }
 })
 
